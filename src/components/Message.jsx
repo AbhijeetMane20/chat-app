388374c760907b71bd83d76da752f46a25b11ad5
Abhijeet <abhijeetmane536@gmail.com>
2023-11-03 19:32:17 +0530

    [abhi] refactored photo url

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { useAppStore } from "../store/AppStore";
 export default function Message({ text, user, photoURL }) {
-  const user = useAppStore((state) => state.user);
+  const userState = useAppStore((state) => state.user);
   return (
     <div
       className="message-container"
       style={{ justifyContent: user === "me" ? "end" : "" }}
     >
       <div>
-        {user === "me" && <img src={user.photoURL} />}
+        {user === "me" && <img src={userState.photoURL} />}
         <label>{text} </label>
         {user !== "me" && <img src={photoURL} />}
       </div>
